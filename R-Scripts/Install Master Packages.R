9864944def028b9f737d954664ad7e436685c8d0
Oanderso <46537127+Oanderso@users.noreply.github.com>
2022-05-06 15:31:09 -0600

    Uploaded initial files.

---
+++ R-Scripts/Install Master Packages.R
@@ -0,0 +1,33 @@
+
+
+#install.packages("cluster")    # clustering algorithms
+#install.packages("factoextra") # clustering visualization
+
+install.packages("dendextend") # for comparing two dendrograms
+install.packages("devtools")
+install.packages("lattice")
+install.packages("multcomp")
+install.packages("sandwich")
+install.packages("ggplot2")
+install.packages("vegan")
+install.packages("CCA")
+install.packages("tidyr")
+install.packages("CCP")
+install.packages("GGally")
+# note that function VIF is in library car
+install.packages("car") #for multicollinearity checks
+install.packages("caret")
+install.packages("dplyr")
+install.packages("phyloseq")
+install.packages("data.table")
+install.packages("gridExtra")
+install.packages("corrplot")
+install.packages("scatterplot3d")
+#for indicator analysis:
+install.packages("labdsv")
+install.packages("vegan")
+install.packages("pheatmap")
+
+install.packages("taxa")
+
+  
